ddd022d390c8beacd52aff0725150edddf7038eb
shahzod <shahzodnematov33@gmail.com>
2022-03-31 23:42:17 +0500

    Add js

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//class work
+let x = 5;
+console.log(x);
+
+x = 7;
+console.log(x);
+
+let a = 1, y = 2, z = 3;
+console.log(x,y,z);
+
+let go = 3;
+console.log(go);
+
+let hot = 10; dot = 11; tot = 12;
+console.log(hot, dot, tot);
+
+let boat = 20; 
+console.log(boat);
+
+boat = 30;
+console.log(boat);
+
+let ok = 24;
+console.log(ok);
+
+//data types
+let myName = "Shahzod"; //string
+let myAge = 18; //number
+let favoriteNumber = 777n; //bigInt
+let isMarried = false; //boolean (false|true)
+let aboutMe; //undefined
+let myNumber = null; //null
+let myJob = Symbol("programmer"); //symbol
+let i = {
+    username: "Shahzod",
+    age: 18,
+    address: "Tashkent",
+    "is married": false,
+}; //Object 
+
+console.log(i);
+
